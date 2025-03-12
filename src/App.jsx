8ec1c84fa1bf13550df7680ab6e63d83969a4167
sheldon-groeneveld <sheldongroeneveld@gmail.com>
2025-03-12 15:09:42 -0600

    add basic room functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,16 @@ const socket = io.connect("http://localhost:8080");
 function App() {
   const [message, setMessage] = useState("");
   const [recievedMessage, setRecievedMessage] = useState("");
+  const [room, setRoom] = useState("");
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
 
   const sendMessage = () => {
-    socket.emit("send_message", { message });
+    socket.emit("send_message", { message, room });
   };
 
   useEffect(() => {
@@ -19,17 +26,28 @@ function App() {
   }, [socket]);
 
   return (
-    <div>
-      <input
-        placeholder="Message..."
-        onChange={(event) => {
-          setMessage(event.target.value);
-        }}
-      />
-      <button onClick={sendMessage}>Send Message</button>
-      <h1>Message: </h1>
-      {recievedMessage}
-    </div>
+    <section>
+      <div>
+        <input
+          placeholder="Room ID..."
+          onChange={(event) => {
+            setRoom(event.target.value);
+          }}
+        />
+        <button onClick={joinRoom}>Join Room</button>
+      </div>
+      <div>
+        <input
+          placeholder="Message..."
+          onChange={(event) => {
+            setMessage(event.target.value);
+          }}
+        />
+        <button onClick={sendMessage}>Send Message</button>
+        <h1>Message: </h1>
+        {recievedMessage}
+      </div>
+    </section>
   );
 }
 

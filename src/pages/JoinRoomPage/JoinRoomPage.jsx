4161191badc5dedfe168ba68c181b3691230da8a
sheldon-groeneveld <sheldongroeneveld@gmail.com>
2025-03-13 11:49:23 -0600

    set join room code char limit

--- src/pages/JoinRoomPage/JoinRoomPage.jsx
+++ src/pages/JoinRoomPage/JoinRoomPage.jsx
@@ -2,7 +2,7 @@ function JoinRoomPage() {
   return (
     <div>
       <h1>Join Room Page Placeholder</h1>
-      <input type="text" placeholder="Enter Room Code" />
+      <input type="text" placeholder="Enter Room Code" maxLength={4} />
       <button>Join Game</button>
     </div>
   );

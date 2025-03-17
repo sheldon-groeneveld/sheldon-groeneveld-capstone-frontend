6b138977c0ec924afa219dc40bbc7505307ad4e1
sheldon-groeneveld <sheldongroeneveld@gmail.com>
2025-03-17 13:00:02 -0600

    fix original array changing order

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -6,7 +6,7 @@ function GamePage() {
   const [shuffledAnswers, setShuffledAnswers] = useState([]);
 
   function shuffle(array) {
-    let tempArray = array;
+    let tempArray = [...array];
     let currentIndex = tempArray.length;
     while (currentIndex != 0) {
       let randomIndex = Math.floor(Math.random() * currentIndex);

018314ac6f8267c8d64dabdcefd07377f4e0653a
sheldon-groeneveld <sheldongroeneveld@gmail.com>
2025-03-20 17:21:45 -0600

    attempt to fix deployement

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,9 @@
 import { io } from "socket.io-client";
 
-const URL =
-  process.env.NODE_ENV === "production"
-    ? "https://sheldon-groeneveld-capstone-api.onrender.com"
-    : "http://localhost:8080";
+const URL = "https://sheldon-groeneveld-capstone-api.onrender.com";
+// process.env.NODE_ENV === "production"
+//   ? "https://sheldon-groeneveld-capstone-api.onrender.com"
+//   : "http://localhost:8080";
 
 export const socket = io(URL, {
   autoConnect: false,
